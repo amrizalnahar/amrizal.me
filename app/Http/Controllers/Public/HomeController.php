5633d91cc2208e0dfe2f99db5250e0e40c67a6b7
amrizal.nahar <amrizal.nahar@tonjoo.com>
2026-05-09 10:33:00 +0700

    fix: eager-load technologies and remove unused profile var

--- app/Http/Controllers/Public/HomeController.php
+++ app/Http/Controllers/Public/HomeController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
-use App\Models\Profile;
 use App\Models\Project;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        $profile = Profile::getProfile();
-        $featuredProjects = Project::published()->ordered()->take(3)->get();
+        $featuredProjects = Project::published()
+            ->with('technologies')
+            ->ordered()
+            ->take(3)
+            ->get();
 
         return view('pages.home', [
-            'profile' => $profile,
             'featuredProjects' => $featuredProjects,
             'activeNav' => 'home',
         ]);
